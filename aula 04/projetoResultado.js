3d24c02b66927c507452f1cc1aed98c4548108fa
João Vitor Reimberg <80371176+JhonReimberg@users.noreply.github.com>
2022-03-06 21:23:27 -0300

    separei o arquivo JS

---
+++ aula 04/projetoResultado.js
@@ -0,0 +1,28 @@
+function resultado() {
+
+    var nome = window.document.getElementById('caixaNome');
+    var idade = window.document.getElementById('suaIdade');
+    var idade2 = window.document.getElementById('idadeAmigo');
+    var calcular = window.document.getElementById('calcular');
+    var calcular2 = window.document.getElementById('calcular2');
+    var pegaValorIdade = Number(idade.value);
+    var pegaValorIdade2 = Number(idade2.value);
+    var pegaNome = (nome.value);
+    var soma2 = Math.abs(pegaValorIdade - pegaValorIdade2);
+
+    calcular.innerHTML = (`${pegaNome}, você tem ${pegaValorIdade} anos de idade.`);
+
+    if (pegaValorIdade < pegaValorIdade2) {
+        calcular2.innerHTML = (`E você é ${soma2} anos mais novo que o seu amigo.`)
+
+    }
+    else if (pegaValorIdade == pegaValorIdade2) {
+        calcular2.innerHTML = ("E você tem a mesma idade que o seu amigo.")
+    }
+    else {
+        calcular2.innerHTML = (`E você é ${soma2} anos mais velho que o seu amigo.`)
+
+
+    }
+
+}
